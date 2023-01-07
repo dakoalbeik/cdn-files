efbf77ef5d7d6e6e0965df227d2e48216f415c22
Dako Albeik <dako.albeik@gmail.com>
2023-01-07 15:33:39 -0600

    Move unit creation to github cdn

--- script_active1.js
+++ script_active1.js
@@ -167,4 +167,164 @@ async function closeWorkorders() {
   }
 }
 
+/**************************************************************************/
+/******************CODE BELOW IS TO DISPLAY THE UNTIS UI ******************/
+/**************************************************************************/
+if (window.location.pathname === "/80245lindsey/pages/SiteDashboard.aspx") main();
+
+
+async function main() {
+  document.addEventListener("keydown", ({
+    key
+  }) => {
+    // to fetch available apts
+    if (key === "+") {
+      __doPostBack('AvailableUnitsDash', '')
+    }
+
+  })
+  if (shouldShowPopup()) {
+    const apts = getApartmentsTable();
+    const notes = getNotesFromStorage();
+    createUI(apts, notes);
+  }
+}
+
+function shouldShowPopup() {
+  const middleTab = document.getElementById('YsiTabStrip1_tab1')
+  return (middleTab?.innerText.includes("Available Units") &&
+    middleTab?.classList.contains('TabStripActiveCell'));
+}
+
+function getNotesFromStorage() {
+  return JSON.parse(localStorage.getItem("__NOTES")) || {};
+}
+
+function setNotesToStorage(obj) {
+  localStorage.setItem("__NOTES", JSON.stringify(obj))
+}
+
+function updateStorage(address, text) {
+  setNotesToStorage({
+    ...getNotesFromStorage(),
+    [address]: text
+  })
+}
+
+function createUI(apts, notes) {
+
+  addStylesToDOM(STYLES);
+
+  const apartmentsElem = createElement('div', "my_custom_wrapper", "", document.body)
+
+  createElement('button', '__close', "✕", apartmentsElem).addEventListener('click', () => {
+    apartmentsElem.style.display = "none";
+  })
+
+  const updateStorageBtn = createElement('button', '__close', `00`, apartmentsElem);
+  updateStorageBtn.addEventListener('click', () => deleteAccessNotes(apts));
+  updateStorageBtn.style.transform = "translateX(-100%)";
+
+  let lastOneBed = false;
+  apts.forEach((apt, i) => {
+    const aptElem = document.createElement('div');
+    aptElem.classList.add('single_unit');
+
+    // work on card here
+    const inner_e = createElement('div', '__inner', "", aptElem),
+      face_e = createElement('div', '__face', "", inner_e),
+      back_e = createElement('div', '__back', "", inner_e);
+
+    const spans = [{
+        css_class: "br",
+        text: `${apt.br}/${apt.sqft.trim() === "1,093" ? 2 : 1}`
+      },
+      {
+        css_class: "unit",
+        text: `${apt.unit.slice(0, 5)}-${apt.unit.slice(5)}`
+      },
+      {
+        css_class: "rent",
+        text: `$${apt.rent}`
+      }, {
+        css_class: "date",
+        text: apt.date
+      },
+      {
+        css_class: "golf",
+        text: apt.isGolf ? "⛳" : `🅿️`
+      }
+    ]
+
+    apt.isUpstairs && spans.push({
+      css_class: 'stairs',
+      text: "𓊍"
+    })
+
+    spans.forEach(({
+      css_class,
+      text
+    }) => createElement('span', `__${css_class}`, text, face_e))
+
+    const input_e = createElement('textarea', '__input', '', back_e);
+    input_e.value = notes[apt.unit] || "";
+    input_e.addEventListener('input', ({
+      target
+    }) => updateStorage(apt.unit, target.value))
+
+    apartmentsElem.appendChild(aptElem);
+    if (apts[i].br === "1" && apts[i + 1]?.br !== "1" && !lastOneBed) {
+      lastOneBed = true;
+      createElement('div', '__divider', "", apartmentsElem);
+    }
+  })
+
+}
+
+function createElement(tag, css_class, text, parent) {
+  const element = document.createElement(tag);
+  element.classList.add(css_class);
+  element.innerText = text;
+  return parent.appendChild(element);
+}
+
+function getApartmentsTable() {
+  const availableAptsTbl = document.getElementById('DashboardGrid_DataTable');
+  const body = availableAptsTbl.firstElementChild;
+  const headers = ['unit', '', 'br', 'rent', 'date', "", "sqft", "occ"]
+  const apts = []
+  for (let row = body.firstElementChild; row; row = row.nextElementSibling) {
+    const apt = {}
+    for (let i = 0; i < headers.length; i++) {
+      if (headers[i] !== "") {
+        apt[headers[i]] = row.children[i].firstElementChild.innerText.trim()
+      }
+    }
+    apt.isUpstairs = !(apt.unit[5] % 2);
+    apt.isGolf = row.children[8].firstElementChild.innerText.includes('Golf');
+    !apt.occ.includes('Eviction') && apts.push(apt);
+  }
+
+  apts.sort((a, b) => (parseFloat(a.rent.replace(/,/g, '')) - parseFloat(b.rent.replace(/,/g, ''))));
+
+  return apts;
+}
+
+function deleteAccessNotes(apts) {
+  const notes = getNotesFromStorage();
+  const newNotes = {};
+  apts.forEach(({
+    unit
+  }) => {
+    if (notes[unit]) {
+      newNotes[unit] = notes[unit]
+    }
+  })
+  setNotesToStorage(newNotes)
+}
+
+function addStylesToDOM(string) {
+  document.head.innerHTML += `<style>${string}</style>`
+}
+
 
